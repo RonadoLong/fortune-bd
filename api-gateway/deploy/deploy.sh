839502df617deeb1e08f957154bbd1845dcf8583
ronado <>
2021-08-01 21:39:40 +0800

    U: update k8s

--- api-gateway/deploy/deploy.sh
+++ api-gateway/deploy/deploy.sh
@@ -1,4 +1,5 @@
 APP_VERSION=v1.0.0
+export KUBECONFIG=/home/admin.conf
 kubectl get no
 sed -i "s/VERSION_NUMBER/${APP_VERSION}/g" api-gateway/deploy/k8s-deployment.yml
 kubectl apply -f api-gateway/deploy/k8s-deployment.yml --namespace=develop
